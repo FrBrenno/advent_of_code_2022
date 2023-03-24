382ad1980196573bde8d1ddc2f1fb503419458ac
FrBrenno <fr.brenno@gmail.com>
2023-03-24 22:51:05 +0100

    Optimisation of the solution day 4

--- day_4.py
+++ day_4.py
@@ -13,20 +13,18 @@ with open(r"./data/input_4.txt") as input:
         upperbound_1, upperbound_2 = int(upperbound_1), int(upperbound_2)
         if lowerbound_1 == lowerbound_2:
             subsets += 1
-            overlaps += 1
         else:
             if lowerbound_1 < lowerbound_2:  # assignment 1 is bigger than assignement 2
                 if upperbound_1 >= upperbound_2:
                     subsets += 1
-                    overlaps += 1
                 elif lowerbound_2 <= upperbound_1:
                     overlaps += 1
             else:  # assignment 2 is bigger than assignement 1
                 if upperbound_2 >= upperbound_1:
                     subsets += 1
-                    overlaps += 1
                 elif upperbound_2 >= lowerbound_1:
                     overlaps += 1
+        overlaps += subsets
 
     print(f"Total subsets: {subsets}")
     print(f"Total overlaps: {overlaps}")
